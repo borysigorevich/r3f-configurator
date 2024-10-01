1a95ccac5bb519251001f8d40433e9ac9be0f4ff
borysigorevich <borysigorevich@gmail.com>
2024-10-01 15:27:42 +0300

    add ruberoid-1000-1000-2

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { Balk1000 } from './balk-1000.tsx';
 import { Balk2200 } from './balk-2200.tsx';
 import { BalkCorner } from './balk-corner.tsx';
-import {Lodge150_50_1000} from "./lodge-150-50-1000.tsx";
-import {Lodge150_50_200} from "./lodge-150-50-200.tsx";
+import { Lodge150_50_1000 } from './lodge-150-50-1000.tsx';
+import { Lodge150_50_200 } from './lodge-150-50-200.tsx';
 import { Lodge20_190_1000 } from './lodge-20-190-1000.tsx';
 import { Lodge20_200_1000 } from './lodge-20-200-1000.tsx';
+import { Ruberoid1000_1000_2 } from './ruberoid-1000-1000-2.tsx';
 
 export const Experience = () => {
 	return (
@@ -17,6 +18,7 @@ export const Experience = () => {
 			<Lodge20_190_1000 />
 			<Lodge150_50_1000 />
 			<Lodge150_50_200 />
+			<Ruberoid1000_1000_2 />
 		</>
 	);
 };
