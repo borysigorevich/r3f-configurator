e618079a936922205992c1c71d13bd3260cf1287
borysigorevich <borysigorevich@gmail.com>
2024-10-01 15:10:23 +0300

    add lodge 20x200x100

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Balk1000 } from './balk-1000.tsx';
 import { Balk2200 } from './balk-2200.tsx';
 import { BalkCorner } from './balk-corner.tsx';
+import {Lodge20_200_1000} from "./lodge-20-200-1000.tsx";
 
 export const Experience = () => {
 	return (
@@ -9,6 +10,7 @@ export const Experience = () => {
 			<Balk1000 />
 			<Balk2200 />
 			<BalkCorner />
+			<Lodge20_200_1000/>
 		</>
 	);
 };
---
+++ src/components/lodge-20-200-1000.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useBasicModelSetup } from '../hooks/useBasicModelSetup.ts';
+
+export const Lodge20_200_1000 = () => {
+	const { ref, object } = useBasicModelSetup({
+		materialsUrl: '/models/Lodge_20x200x1000.mtl',
+		mapUrl: '/textures/texture_wood.jpg',
+		normalMapUrl: '/textures/texture_wood_normal.jpg',
+		objectUrl: '/models/Lodge_20x200x1000.obj',
+	});
+
+	return (
+		<>
+			<primitive object={object} ref={ref}
+				position={[1, 0, 0]}
+				/>
+		</>
+	);
+};
