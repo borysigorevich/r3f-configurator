ce80149bb635182722fa8e7cf67252a14f70cf89
borysigorevich <borysigorevich@gmail.com>
2024-10-01 14:37:42 +0300

    add balk 2200

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Balk1000 } from './balk-1000.tsx';
+import { Balk2200 } from './balk-2200.tsx';
 
 export const Experience = () => {
 	return (
 		<>
 			<Balk1000 />
+			<Balk2200 />
 		</>
 	);
 };
