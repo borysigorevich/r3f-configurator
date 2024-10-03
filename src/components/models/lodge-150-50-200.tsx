18929500cbc549ce64b49f3e3a4e7ba23fd38fae
borysigorevich <borysigorevich@gmail.com>
2024-10-03 18:00:23 +0300

    update Lodge150_50_200

--- src/components/models/lodge-150-50-200.tsx
+++ src/components/models/lodge-150-50-200.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { useBasicModelSetup } from '../../hooks/useBasicModelSetup.ts';
+import { BasicModelProps } from '../../types.ts';
 
-export const Lodge150_50_200 = () => {
+type Balk150_150_200Props = BasicModelProps;
+
+export const Lodge150_50_200 = ({
+	children,
+	scale,
+	rotation,
+	position,
+}: Balk150_150_200Props) => {
 	const { ref, object } = useBasicModelSetup({
 		materialsUrl: '/models/lodge_150x50x200.mtl',
 		mapUrl: '/textures/texture_wood.jpg',
@@ -10,8 +18,9 @@ export const Lodge150_50_200 = () => {
 	});
 
 	return (
-		<>
-			<primitive object={object} ref={ref} />
-		</>
+		<group position={position}>
+			<primitive object={object} ref={ref} scale={scale} rotation={rotation} />
+			{children}
+		</group>
 	);
 };
